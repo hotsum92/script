b6e4a1ee8d4ba2eefe68eb0bdbcb5734b9b5c3e4
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-07-01 10:03:36 +0900

    add update

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 basher install hotsum92/md-link
 basher install hotsum92/bash-today
 basher install hotsum92/bash-eigiro
+
+basher upgrade hotsum92/md-link
+basher upgrade hotsum92/bash-today
+basher upgrade hotsum92/bash-eigiro
