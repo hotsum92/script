6c107f82cf86724d85c5c61ccfa8b9c7e1d4b152
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-07-04 00:23:36 +0900

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 basher install hotsum92/md-link
 basher install hotsum92/bash-today
 basher install hotsum92/bash-eigiro
+basher install hotsum92/bash-plot
 
 basher upgrade hotsum92/md-link
 basher upgrade hotsum92/bash-today
 basher upgrade hotsum92/bash-eigiro
+basher upgrade hotsum92/bash-plot
