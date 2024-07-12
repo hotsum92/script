86d0ec17f8e95de5685c2b5c697580eaedef293e
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-07-12 16:19:56 +0900

    add bash-cache

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ basher install hotsum92/bash-today
 basher install hotsum92/bash-eigiro
 basher install hotsum92/bash-plot
 basher install hotsum92/go-stat
+basher install hotsum92/bash-cache
 
 basher upgrade hotsum92/md-link
 basher upgrade hotsum92/bash-today
 basher upgrade hotsum92/bash-eigiro
 basher upgrade hotsum92/bash-plot
 basher upgrade hotsum92/go-stat
+basher upgrade hotsum92/bash-cache
